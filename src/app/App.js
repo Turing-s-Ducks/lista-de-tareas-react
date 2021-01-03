92350aa1af74294c686961544d95f06902c2589e
Martin <alquimista3301@gmail.com>
2021-01-03 00:44:50 -0600

    fix routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,9 @@
 import { BrowserRouter, Redirect, Route} from 'react-router-dom';
-import Login from './components/login/Login';
-import Todo from './components/lista-tareas/Todo'
+import Todo from './components/lista-tareas/Todo';
 function App() {
   return (
     <BrowserRouter>
-      <Route path='/' exact component={Login} />
-      <Route path='/analytics' component={Login} />
-      <Route path='/notes' component={Login} />
+      <Route path='/' exact component={Todo} />
       <Route path='/todo' component={Todo} />
       {/* <Redirect from='' to='/' /> */}
     </BrowserRouter>
